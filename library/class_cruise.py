c8663634dd643d7a524f97e38299eab606b29fc7
agostina nobo <agosnobo@yahoo.com>
2023-08-12 16:07:04 -0300

    cruise calculate_weight

--- library/class_cruise.py
+++ library/class_cruise.py
@@ -3,3 +3,7 @@ from class_ship import ship
 class cruise(ship):
     def __init__(self, passengers, draft, crew):
         self.passengers = passengers
+
+    def calculate_weight(self):
+        weight=self.draft-self.crew*1.5-self.passengers*2.25
+        return weight
