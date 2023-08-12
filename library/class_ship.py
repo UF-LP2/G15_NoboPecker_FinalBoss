2c99a821e8c29eafdca441c41482127b5076d887
Paula Pecker <paulapeckerfasce@gmail.com>
2023-08-12 16:01:49 -0300

    claculate weight y is worth it

--- library/class_ship.py
+++ library/class_ship.py
@@ -2,3 +2,13 @@ class ship:
     def _init__(self, draft, crew):
         self.draft = draft
         self.crew = crew
+    def is_worth_it(self):
+        aux=self.calculate_weight
+        if(aux>20):
+            #saquear
+        else:
+            #exc
+
+    def calculate_weight(self):
+        weight=self.draft-self.crew*1.5
+        return weight
