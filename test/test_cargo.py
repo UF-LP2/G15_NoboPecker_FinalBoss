997b3215251d8c5a670aa70921d394569a7a9d85
agostina nobo <agosnobo@yahoo.com>
2023-08-15 12:44:34 -0300

    test cargo

--- test/test_cargo.py
+++ test/test_cargo.py
@@ -1,7 +1,7 @@
 import pytest
 from src.ships import Cargo
 def test_is_worth_it() -> None:
-
+    #todoOk
     cargo1 = Cargo(1200, 500, 1, 0.5)
     cargo2 = Cargo(150, 25, 2, 0.25)
     cargo3 = Cargo(1000, 200, 10, 1)
@@ -9,13 +9,19 @@ def test_is_worth_it() -> None:
     assert cargo2.is_worth_it() == 111.5
     assert cargo3.is_worth_it() == 665
 
+    #menor a 20
     cargo1.draft=0
     cargo2.draft=10
     cargo3.draft=100
     with pytest.raises(ValueError):
-        assert (cargo1.is_worth_it() < 20) == True
-        assert (cargo2.is_worth_it() < 20) == True
-        assert (cargo3.is_worth_it() < 20) == True
+        cargo1.is_worth_it()
+    with pytest.raises(ValueError):
+        cargo2.is_worth_it()
+    with pytest.raises(ValueError):
+        cargo3.is_worth_it()
+
+def test_constructor() -> None:
+
 
 
 
