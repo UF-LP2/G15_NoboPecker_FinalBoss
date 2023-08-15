619ed2a3c873270f41a2e7089a70db6302746fba
Paula Pecker <paulapeckerfasce@gmail.com>
2023-08-15 16:25:02 -0300

    test

--- test/test_cruises.py
+++ test/test_cruises.py
@@ -1,25 +1,24 @@
 
 import pytest
 from src.ships import Cruise
-
-
-def test_is_worth_it():
+def test_crucero_1():
    #todo ok
-   crucero1=Cruise(2100,600,90)
-   crucero2=Cruise(500,100,83)
-   crucero3=Cruise(300,56,20)
-   assert crucero1.is_worth_it()==997.5
-   assert crucero2.is_worth_it()==163.25
-   assert crucero3.is_worth_it() == 171
-
-#menor a 20
-   crucero1.draft=-10
-   crucero2.draft=15
-   crucero3.draft=80
-
+   crucero1 = Cruise(2100, 600, 90)
+   assert crucero1.is_worth_it()>=20
+   # menor a 20
+   crucero1.draft = 10
    with pytest.raises(ValueError):
       crucero1.is_worth_it()
+def test_crucero_2():
+   crucero2 = Cruise(500, 100, 83)
+   assert crucero2.is_worth_it() >=20
+   crucero2.draft = 15
    with pytest.raises(ValueError):
       crucero2.is_worth_it()
+def test_crucero_3():
+   crucero3=Cruise(300,56,20)
+   assert crucero3.is_worth_it()>=20
+   crucero3.draft=8
    with pytest.raises(ValueError):
       crucero3.is_worth_it()
+
--- test/test_ships.py
+++ test/test_ships.py
@@ -1,6 +1,28 @@
 import pytest
 from src.ships import Ship
-def test_calculateweight(self) -> None:
-    barco= Ship(1200,500)
-    assert barco.calculate_weight()== 450
+def test_barco_1():
+    barco1=Ship(122,56)
+    #todo ok
+    assert barco1.is_worth_it()>=20
+    #menor a 20
+    barco1.draft=10
+    with pytest.raises(ValueError):
+        barco1.is_worth_it()
+def test_barco_2():
+    barco2=Ship(500,120)
+    #todo ok
+    assert barco2.is_worth_it()>=20
+    #menor a 20
+    barco2.draft=30
+    with pytest.raises(ValueError):
+        barco2.is_worth_it()
+
+def test_barco_3():
+    barco3=Ship(900,600)
+    #todo ok
+    assert barco3.is_worth_it()>=20
+    #menor a 20
+    barco3.draft=500
+    with pytest.raises(ValueError):
+        barco3.is_worth_it()
 
