2e2d9cf08de3078ccf06527f11761f5afedc4c1b
agostina nobo <agosnobo@yahoo.com>
2023-08-15 10:07:09 -0300

    statics cargo

--- src/class_cargo.py
+++ src/class_cargo.py
@@ -1,16 +1,20 @@
 from class_ship import ship
 class cargo (ship):
+    # statics de los valores correspondientes a cada calidad de cargo
+    value_quality1 = 3.5
+    value_quality05 = 2
+    value_quality025 = 0.5
     def __init__(self, cargo, quality, draft, crew):
         self.cargo = cargo
         self.quality = quality
     def calculate_weight(self):
         cargo_aux=0
         if(self.quality==1):
-            cargo_aux=3.5
+            cargo_aux= self.value_quality1
         elif (self.quality==0.5):
-            cargo_aux=2
+            cargo_aux= self.value_quality05
         elif(self.quality==0.25):
-            cargo_aux=0.5
+            cargo_aux= self.value_quality025
         else:
             raise ValueError("Valor de calidad invalido")
 
